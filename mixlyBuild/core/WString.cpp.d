5b72638f3d55e6986ab1661e750a24881612d770
zsqiang <898646020@qq.com>
2022-07-06 00:11:18 +0800

    修改首页

--- mixlyBuild/core/WString.cpp.d
+++ mixlyBuild/core/WString.cpp.d
@@ -1,3 +1,3 @@
-D:\обть\Mixly-master\mixlyBuild\core\WString.cpp.o: \
- D:\обть\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\WString.cpp \
- D:\обть\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\WString.h
+D:\Mixly\mixlyBuild\core\WString.cpp.o: \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\WString.cpp \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\WString.h
--- mixlyBuild/sketch/testArduino.ino.cpp.d
+++ mixlyBuild/sketch/testArduino.ino.cpp.d
@@ -1,16 +1,13 @@
-D:\下载\Mixly-master\mixlyBuild\sketch\testArduino.ino.cpp.o: \
- D:\下载\Mixly-master\mixlyBuild\sketch\testArduino.ino.cpp \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Arduino.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/binary.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/WCharacter.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/WString.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/HardwareSerial.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Stream.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Print.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Printable.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/USBAPI.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Arduino.h \
- D:\下载\Mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\variants\eightanaloginputs/pins_arduino.h \
- d:\下载\mixly-master\arduino\portable\packages\arduino\hardware\avr\1.8.1\variants\standard\pins_arduino.h \
- D:\下载\Mixly-master\arduino\portable\sketchbook\libraries\Servo\src/Servo.h \
- D:\下载\Mixly-master\arduino\portable\sketchbook\libraries\Servo\src/avr/ServoTimers.h
+D:\Mixly\mixlyBuild\sketch\testArduino.ino.cpp.o: \
+ D:\Mixly\mixlyBuild\sketch\testArduino.ino.cpp \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Arduino.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/binary.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/WCharacter.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/WString.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/HardwareSerial.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Stream.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Print.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Printable.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/USBAPI.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino/Arduino.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\variants\standard/pins_arduino.h
