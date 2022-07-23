ee8f49d445b1fa7ded7a19936c5efb1b39e34d5f
张守强 <898646020@qq.com>
2022-07-23 20:09:09 +0800

    合并

---
+++ mixlyBuild/core/CDC.cpp.d
@@ -0,0 +1,12 @@
+D:\Mixly\mixlyBuild\core\CDC.cpp.o: \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\CDC.cpp \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\USBAPI.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\Arduino.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\binary.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\WCharacter.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\WString.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\HardwareSerial.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\Stream.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\Print.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\Printable.h \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\variants\standard/pins_arduino.h
---
+++ mixlyBuild/core/new.cpp.d
@@ -0,0 +1,2 @@
+D:\Mixly\mixlyBuild\core\new.cpp.o: \
+ D:\Mixly\arduino\portable\packages\arduino\hardware\avr\1.8.1\cores\arduino\new.cpp
